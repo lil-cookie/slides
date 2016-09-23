cd559bbba5818600a32dcf3cf8cdc5ace355fd10
lil-cookie <cookie@ro.ru>
2016-09-23 12:23:56 +0400

    add slide2

--- js/slide2.js
+++ js/slide2.js
@@ -1,6 +1,7 @@
-$(".item").children().clone()               // сделаем копию элемента hello
+$(document).ready(function()
+{$(".item").children().clone()               // сделаем копию элемента hello
  //  .text("И снова здравствуйте!")  изменим текст внутри нее
 .appendTo("#slide2");          // вставим измененный элемент в конец элемента container
 
 $("#slide2").attr('id','locked');         // добавим этой копии класс newElement
-
+})
