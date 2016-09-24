f05b9158b31789fa62f16043ee1273350d0d3dd1
lil-cookie <cookie@ro.ru>
2016-09-24 19:07:59 +0400

    done

--- js/roller.js
+++ js/roller.js
@@ -1,16 +1,16 @@
 
 $(document).ready(function(){
   $( "#rollerContainer" ).slider({
-  value : 1,//Значение, которое будет выставлено слайдеру при загрузке
-  min : 1,//Минимально возможное значение на ползунке
-  max : 21,//Максимально возможное значение на ползунке
-  step : 1,//Шаг, с которым будет двигаться ползунок
+  value : 1,//значение, которое будет выставлено слайдеру при загрузке
+  min : 1,//минимально возможное значение на ползунке
+  max : 21,//максимально возможное значение на ползунке
+  step : 1,//шаг, с которым будет двигаться ползунок
   create: function( event, ui ) {
-   val = $( "#rollerContainer" ).slider("value");//При создании слайдера, получаем его значение в перемен. val
-  $( "#contentRoller").html( val );//Заполняем этим значением элемент с id contentSlider
+   val = $( "#rollerContainer" ).slider("value");//при создании слайдера, получаем его значение в перемен. val
+  $( "#contentRoller").html( val );//заполняем этим значением элемент с id contentRoller
  },
  slide: function( event, ui ) {
-  $( "#contentRoller" ).html( ui.value );//При изменении значения ползунка заполняем элемент с id contentSlider
+  $( "#contentRoller" ).html( ui.value );//при изменении значения ползунка заполняем элемент с id contentRoller
   redBox(ui.value);          
             }
         });
@@ -19,10 +19,10 @@ $(document).ready(function(){
 var oldValue=1;
 countMargin = 0;
 
+//функция перемещения красного квадрата
 function redBox(val)
 {
- countMargin= (val-1)*46;
-
+ countMargin= (val-1)*46; //46px - длина одного шага
  $(".redBoxContainer").animate({
             marginLeft: countMargin+"px"
         },20);
--- js/updateSlide2.js
+++ js/updateSlide2.js
@@ -1,3 +1,6 @@
+/*
+файл отвечает за  обновление контента на втором слайде.
+*/
 window.onload=loop;
 var next ;
 var prev ;
@@ -6,16 +9,19 @@ function loop()
 {
 next = document.getElementsByClassName("next")[0];
 prev = document.getElementsByClassName("prev")[0];
+//вешаем Listener'ов на событие "клик" по стрелокам- навигаторам
 next.addEventListener("click",change,false);
 prev.addEventListener("click",change,false);
 }
 
-
+//функция, срабатывающая при нажатии на стрелки смены слайдов
 function change() 
 {
-	console.log("tytyutf");
+	//изменяем стиль блока с дедом на текущий на текущий на 1 слайде
 	$('#pacient-2')[0].style.cssText = $('#pacient-1')[0].style.cssText;
+	//изменяем стиль блока с ползунком на текущий на 1 слайде
 	$('#rollerPosition-2')[0].style.cssText = $('#rollerPosition-1')[0].style.cssText;
+	//изменяем стиль блока с таблеткой на текущий на 1 слайде
 	$('#pill-2')[0].style.cssText = $('#pill-1')[0].style.cssText;
 }
 
